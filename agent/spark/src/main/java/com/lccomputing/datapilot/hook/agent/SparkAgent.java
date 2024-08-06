1afab30a7f9cf39f92846b30c3fc1190c603d5e3
DiHua <dihua@lccomputing.com>
2024-08-06 06:15:39 +0000

    Fix dev

--- agent/spark/src/main/java/com/lccomputing/datapilot/hook/agent/SparkAgent.java
+++ agent/spark/src/main/java/com/lccomputing/datapilot/hook/agent/SparkAgent.java
@@ -1,3 +1,19 @@
+/*
+ * Lccomputing Sky DataPilot Hook
+ * Copyright 2021 Lccomputing
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.lccomputing.datapilot.hook.agent;
 
 import java.lang.instrument.Instrumentation;
@@ -6,7 +22,6 @@ public class SparkAgent {
     public static void premain(String args, Instrumentation inst) {
         try {
             inst.addTransformer(new SparkSubmitTrans(), true);
-            inst.addTransformer(new SparkSubmit240Trans(), true);
         } catch (Exception ex) {
             System.err.println("lcc SparkAgent start failed: " + ex.getMessage() + ", premain args: " + args);
         }
--- agent/tez/src/main/java/com/lccomputing/datapilot/hook/agent/DagImplTrans.java
+++ agent/tez/src/main/java/com/lccomputing/datapilot/hook/agent/DagImplTrans.java
@@ -1,3 +1,19 @@
+/*
+ * Lccomputing Sky DataPilot Hook
+ * Copyright 2021 Lccomputing
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.lccomputing.datapilot.hook.agent;
 
 import javassist.ClassPool;
@@ -17,7 +33,7 @@ public class DagImplTrans implements ClassFileTransformer {
     // @formatter:off
     private static final String CODE =
             "try {\n" +
-            "  com.lccomputing.datapilot.hook.agent.DagReporter.report(this, this.getState(), this.finishTime, this.initTime);\n" +
+            "  com.lccomputing.datapilot.hook.agent.DagReporter.report($0, $0.getState(), $0.finishTime, $0.initTime);\n" +
             "} catch (Exception e) {\n" +
             "  LOG.warn(\"LCC Hook DagImplTrans run failed, ignore it and continue: \" + e);\n" +
             "  LOG.debug(\"LCC Hook DagImplTrans run failed, ignore it and continue\", e);\n" +
